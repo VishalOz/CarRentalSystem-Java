d227f5ddecd56444163d6d2e8f87d643d09abb7c
VishalOz <vishal.20240036@iit.ac.lk>
2025-07-07 08:17:21 +0530

    Client

---
+++ src/Client.java
@@ -0,0 +1,18 @@
+public class Client extends User{
+    public Client() {
+        super();
+    }
+    @Override
+    public void showList() {
+        System.out.println("|-------------------------------|");
+        System.out.println("|          Client               |");
+        System.out.println("|-------------------------------|");
+        System.out.println("| 1. View Cars                  |");
+        System.out.println("| 2. Rent Car                   |");
+        System.out.println("| 3. Return Car                 |");
+        System.out.println("| 4. Show My Rents              |");
+        System.out.println("| 5. Edit My Data               |");
+        System.out.println("| 7. Quit                       |");
+        System.out.println("|-------------------------------|");
+    }
+}
